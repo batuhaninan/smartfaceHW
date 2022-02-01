de1fbd1c016794cbe14b3b34ab2f8d0ec704b0a8
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-01 18:21:06 +0300

    commented out navbar and added every screen to stack

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
 
+import Constants from "expo-constants";
+
 import { Provider as PaperProvider } from "react-native-paper";
 
 import Colors from '../constants/Colors';
@@ -16,6 +18,8 @@ import PrincipalScreen from '../screens/PrincipalScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import { Provider } from 'react-native-paper/lib/typescript/core/settings';
+import LandingScreen from "../screens/LandingScreen";
+import SignupScreen from "../screens/SignupScreen";
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -30,61 +34,85 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 }
 
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 function RootNavigator() {
   return (
     <Stack.Navigator>
-        <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-        <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen name="Root" component={LandingScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SignupScreen" component={SignupScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="PrincipalScreen" component={PrincipalScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="StudentScreen" component={StudentScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="TeacherScreen" component={TeacherScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
 }
 
 
-const BottomTab = createBottomTabNavigator<RootTabParamList>();
+// const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
-function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+// function BottomTabNavigator() {
+//   const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="StudentScreen"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
-      <BottomTab.Screen
-        name="StudentScreen"
-        component={StudentScreen}
-        options={({ navigation }: RootTabScreenProps<'StudentScreen'>) => ({
-          title: 'Student',
-          tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
-        })}
-      />
-      <BottomTab.Screen
-        name="TeacherScreen"
-        component={TeacherScreen}
-        options={{
-          title: 'Teacher',
-          tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="PrincipalScreen"
-        component={PrincipalScreen}
-        options={{
-          title: 'Principal',
-          tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
-}
+  // return (
+    // <BottomTab.Screen
+    //   name="Landing"
+    //   component={LandingScreen}
+    //   options={({ navigation }: RootTabScreenProps<'Landing'>) => ({
+    //     title: 'Landing',
+    //     tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
+    //   })}
+    // />
+    // <BottomTab.Screen
+
+        // component={LandingScreen}
+      // />
+    // <BottomTab.Navigator
+    //   initialRouteName="StudentScreen"
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme].tint,
+    //   }}>
+    //   <BottomTab.Screen
+    //     name="Landing"
+    //     component={LandingScreen}
+    //     options={({ navigation }: RootTabScreenProps<'Landing'>) => ({
+    //       title: 'Landing',
+    //       tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
+    //     })}
+    //   />
+    //   <BottomTab.Screen
+    //     name="StudentScreen"
+    //     component={StudentScreen}
+    //     options={({ navigation }: RootTabScreenProps<'StudentScreen'>) => ({
+    //       title: 'Student',
+    //       tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
+    //     })}
+    //   />
+    //   <BottomTab.Screen
+    //     name="TeacherScreen"
+    //     component={TeacherScreen}
+    //     options={{
+    //       title: 'Teacher',
+    //       tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
+    //     }}
+    //   />
+    //   <BottomTab.Screen
+    //     name="PrincipalScreen"
+    //     component={PrincipalScreen}
+    //     options={{
+    //       title: 'Principal',
+    //       tabBarIcon: ({ color }) => <TabBarIcon name="bar-chart-2" color={color} />,
+    //     }}
+    //   />
+    // </BottomTab.Navigator>
+  // );
+// }
 
 
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof Feather>['name'];
-  color: string;
-}) {
-  return <Feather size={30} style={{ marginBottom: -3 }} {...props} />;
-}
+// function TabBarIcon(props: {
+//   name: React.ComponentProps<typeof Feather>['name'];
+//   color: string;
+// }) {
+//   return <Feather size={30} style={{ marginBottom: -3 }} {...props} />;
+// }
