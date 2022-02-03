e7338ee0e7df417ca0594d0b72b0cfe27ec543ca
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 01:16:34 +0300

    removed unnecessary fields from props & refactored for added types

--- components/CourseListByStudentItem.tsx
+++ components/CourseListByStudentItem.tsx
@@ -1,25 +1,36 @@
-import {Checkbox, List, Surface} from "react-native-paper";
-import React, {useEffect, useState} from "react";
+import {List, Surface} from "react-native-paper";
+import React, {useState} from "react";
 import StudentUploadedFileDialog from "./StudentUploadedFileDialog";
+import {HomeworkAndUploadedFiles, UploadedFileByStudent} from "../types";
 
 
-interface CourseListByStudentItem {
-	file: any,
-	course: any,
-	homework: any,
-	teacher: any
+interface CourseListByStudentItemProps {
+	homeworkAndFiles: HomeworkAndUploadedFiles,
 }
 
-const CourseListByStudentItem: CourseListByStudentItem = ({ file, course, homework ,teacher }) => {
+const CourseListByStudentItem: React.FC<CourseListByStudentItemProps> = ({ homeworkAndFiles }) => {
 
 	const [shouldOpenDialog, setShouldOpenDialog] = useState(false);
-	console.log(homework)
+	const [selectedStudentFile, setSelectedStudentFile] = useState<UploadedFileByStudent>();
+
+	const selectStudentFile = (file: UploadedFileByStudent) => {
+		setSelectedStudentFile(file)
+		setShouldOpenDialog(true)
+	}
+
 	return (
 		<Surface>
 			{shouldOpenDialog &&
-          <StudentUploadedFileDialog file={file} closeDialog={setShouldOpenDialog} homework={homework} teacher={teacher} course={course.course} />}
+          <StudentUploadedFileDialog file={selectedStudentFile!} closeDialog={setShouldOpenDialog} />}
 
-			<List.Item key={file.filePath} onPress={() => setShouldOpenDialog(true)} title={file.student} />
+			<List.Accordion title={homeworkAndFiles.homeworkTitle} style={{ backgroundColor: "#eee6e6" }}>
+				{homeworkAndFiles.files.map((homework) => {
+					return (
+							<List.Item key={homework.filePath} onPress={() => selectStudentFile(homework)} title={homework.student} />
+					)
+				})}
+			</List.Accordion>
+
 		</Surface>
 	)
 }
