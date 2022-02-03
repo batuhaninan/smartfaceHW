208fa3eb0a39dc8d08665b3eb3e7dd002890f7cd
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 01:10:58 +0300

    style refactor

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
 import { LogBox } from "react-native";
 
 
