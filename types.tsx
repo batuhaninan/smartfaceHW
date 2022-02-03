1c6f9ac0b9aa480286f38c21870edc17556c1ab7
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 01:13:34 +0300

    removed TabNavigation props & added new interfaces for TeacherScreen

--- types.tsx
+++ types.tsx
@@ -1,17 +1,9 @@
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import firebase from "firebase/compat";
 import QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 import {Course} from "./models/Course";
 import {Teacher} from "./models/Teacher";
 import {Homework} from "./models/Homework";
 
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
-}
 
 export type DocumentResultFixed = {
   name: string;
@@ -23,31 +15,26 @@ export type DocumentResultFixed = {
   output?: FileList | null;
 }
 
-export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
-  NotFound: undefined;
-};
-
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-  RootStackParamList,
-  Screen
->;
-
-export type RootTabParamList = {
-  StudentScreen: undefined;
-  TeacherScreen: undefined;
-  PrincipalScreen: undefined;
-};
-
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-  BottomTabScreenProps<RootTabParamList, Screen>,
-  NativeStackScreenProps<RootStackParamList>
->;
-
 export interface CourseData {
   "courseSnapshot": QueryDocumentSnapshot
   "course": Course
   "teacher": Teacher
   "homeworks": Homework[]
+}
+
+export interface UploadedFileByStudent {
+  student: string,
+  filePath: string
+}
+
+export interface HomeworkAndUploadedFiles {
+  files: UploadedFileByStudent[],
+  homeworkTitle: string
+}
+
+export interface TeacherCourseData {
+  "courseSnapshot": QueryDocumentSnapshot
+  "course": Course
+  "teacher": Teacher
+  "homeworksAndFiles": HomeworkAndUploadedFiles[]
 }
