c8aff596a08edbc7a3d2336cdeb36d564835a9c2
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-03 21:47:14 +0300

    copied old version of student screen, not finished

--- screens/PrincipalScreen.tsx
+++ screens/PrincipalScreen.tsx
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import {CourseData, RootTabScreenProps} from '../types';
+import CourseList from "../components/CourseList";
+import {useEffect, useState} from "react";
+import {getData} from "../utils/FirebaseStorageUtils";
+import {styles} from "../Styles";
 
 
 
@@ -9,30 +13,20 @@ import { RootTabScreenProps } from '../types';
 export default function PrincipalScreen({ navigation }: RootTabScreenProps<'PrincipalScreen'>) {
 
 
-  
+  const [courses, setCourses] = useState<CourseData[]>([]);
+
+  useEffect(() => {
+    getData().then(r => { setCourses(r) })
+  }, [])
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Principal Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+      <CourseList
+        courses={courses}
+      />
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
