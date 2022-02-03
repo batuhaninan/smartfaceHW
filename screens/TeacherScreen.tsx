e86be9c5d5f1a0c9b9e46ed4144b2b8385337138
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-03 21:49:25 +0300

    now gets data on load & passes data to CourseListByStudent component

--- screens/TeacherScreen.tsx
+++ screens/TeacherScreen.tsx
@@ -1,179 +1,30 @@
-import { StyleSheet } from 'react-native';
-
-import { useState } from 'react';
-
-
-import { Text, View } from '../components/Themed';
-
-import HomeworkList from "../components/HomeworkList"
-
-import { Surface, List } from 'react-native-paper';
+import {useEffect, useState} from 'react';
+import { View } from '../components/Themed';
+import { List } from 'react-native-paper';
+import {styles} from "../Styles";
+import {getAllCoursesAndHomeworksOfTeacher} from "../utils/FirebaseStorageUtils";
+import CourseListByStudent from "../components/CourseListByStudent";
 
 
-import { Course, UploadedHomeworksByCourse } from '../types';
-
-
-var data: Course[] = [
-  {
-    "courseName": "Python",
-    "teacher": {
-      "id": 1,
-      "name": "Batuhan Inan",
-      "age": 23,
-    }, 
-    "homeworks": [
-      {
-        "id": 1,
-        "name": "Ödev 1",
-        "studentCount": 159,
-        "totalAttempts": 5,
-        "currentAttempts": 1,
-        "isUploaded": false,
-        "uploadedFileNames": [],
-      },
-      {
-        "id": 1,
-        "name": "Ödev 2",
-        "studentCount": 159,
-        "totalAttempts": 5,
-        "currentAttempts": 1,
-        "isUploaded": false,
-        "uploadedFileNames": [],
-      }
-    ]
-  },
-  {
-    "courseName": "C++",
-    "teacher": {
-      "id": 1,
-      "name": "Batuhan Inan",
-      "age": 23,
-    }, 
-    "homeworks": [
-      {
-        "id": 1,
-        "name": "Ödev 1",
-        "studentCount": 159,
-        "totalAttempts": 5,
-        "currentAttempts": 1,
-        "isUploaded": false,
-        "uploadedFileNames": [],
-      },
-      {
-        "id": 1,
-        "name": "Ödev 2",
-        "studentCount": 159,
-        "totalAttempts": 5,
-        "currentAttempts": 1,
-        "isUploaded": false,
-        "uploadedFileNames": [],
-      }
-    ]
-  },
-]
-
-const hws: UploadedHomeworksByCourse[] = [
-  {
-    "courseName": "Python",
-    "students": [
-      {
-        "studentName": "Batuhan Inan",
-        "uploadedHomeworks": [
-          {
-            "homeworkTitle": "Ödev 1",
-            "file": "dummy.png"
-          },
-          {
-            "homeworkTitle": "Ödev 2",
-            "file": "dummy.png"
-          },
-        ]
-      },
-      {
-        "studentName": "Atilla Başaran",
-        "uploadedHomeworks": [
-          {
-            "homeworkTitle": "Ödev 1",
-            "file": "dummy.png"
-          },
-          {
-            "homeworkTitle": "Ödev 2",
-            "file": "dummy.png"
-          },
-        ]
-      }
-    ]
-  },
-  {
-    "courseName": "C++",
-    "students": [
-      {
-        "studentName": "Batuhan Inan",
-        "uploadedHomeworks": [
-          {
-            "homeworkTitle": "Ödev 1",
-            "file": "dummy.png"
-          },
-          {
-            "homeworkTitle": "Ödev 2",
-            "file": "dummy.png"
-          },
-        ]
-      },
-      {
-        "studentName": "Atilla Başaran",
-        "uploadedHomeworks": [
-          {
-            "homeworkTitle": "Ödev 1",
-            "file": "dummy.png"
-          },
-          {
-            "homeworkTitle": "Ödev 2",
-            "file": "dummy.png"
-          },
-        ]
-      }
-    ]
-  }
-]
 
 export default function TeacherScreen() {
 
+  const [courses, setCourses] = useState<any>([]);
 
-  const [shouldOpenDialog, setShouldOpenDialog] = useState(false);
+  useEffect(() => {
+    getAllCoursesAndHomeworksOfTeacher().then(r => {
+      setCourses(r)
+    })
+
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text>Show teacher's courses</Text>
-      <Text>Then show all homeworks</Text>
-      <Text>Add status to each homework of each student</Text>
-      <Text>Then expand on the homework and see all students uploaded files</Text>
-      
-
-        <HomeworkList 
-          courses={hws}
-        />
-        {/* {hws.map((course) => {
-          return (
-            <List.Accordion title={course.courseName}>
-              {
-                course.homeworks.map((hw) => {
-                  return (
-                    <List.Item title={hw.studentName}>
-                      
-                    </List.Item>
-                  )
-                })
-              }
-            </List.Accordion>
-          )
-        })} */}
+        <List.Section title={"Courses"}>
+          {courses.map((course: any) => {
+            return <CourseListByStudent key={course.course.name} course={course}/>
+          })}
+        </List.Section>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
