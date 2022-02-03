688a95d2afdd128fdde612e59f9cd32b9fb0384d
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-03 21:40:12 +0300

    added dependencies

--- App.tsx
+++ App.tsx
@@ -7,9 +7,11 @@ import Navigation from './navigation';
 
 import { LogBox } from "react-native";
 
+
 export default function App() {
 
-  LogBox.ignoreLogs(["Setting a timer", ".*(AsyncStorage).*"]);
+
+  LogBox.ignoreLogs(["Setting a timer", ".*(AsyncStorage).*", "EventEmitter.removeListener('change', ...): Method has been deprecated. Please instead use `remove()` on the subscription returned by `EventEmitter.addListener`.", "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage", "Overwriting fontFamily style attribute preprocessor"]);
 
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
