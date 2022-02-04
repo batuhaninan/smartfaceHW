bfe9f462ae72bcba96e4530ab334b0e2f523662a
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 15:39:40 +0300

    added key to List.Section

--- components/CourseList.tsx
+++ components/CourseList.tsx
@@ -11,7 +11,7 @@ interface CourseListProps {
 const CourseList: React.FC<CourseListProps> = ({ courses }): JSX.Element => {
 
 	return (
-		<List.Section title="Courses" >
+		<List.Section title="Courses" key={courses.toString()}>
 			{courses.map((courseCollection: CourseData) => {
 				return (
 					<CourseListCourseItem key={courseCollection.courseSnapshot.id} course={courseCollection.course} teacher={courseCollection.teacher} homeworks={courseCollection.homeworks}/>
