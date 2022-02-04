5e009408944d3d548eaad652fe903762528a8828
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 15:39:08 +0300

    removed comments

--- Styles.ts
+++ Styles.ts
@@ -6,9 +6,6 @@ export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		marginTop: Constants.statusBarHeight,
-		// width: "95%",
-		// marginLeft: Dimensions.get("window").width / 10,
-		// marginBottom: Dimensions.get("window").width / 10,
 		borderRadius: 90,
 		backgroundColor: "transparent",
 	},
