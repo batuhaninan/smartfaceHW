100eb2e316c98ad7bf5cfb27f65a619ee26c6668
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 15:39:25 +0300

    added types for principalScreen

--- types.tsx
+++ types.tsx
@@ -3,6 +3,7 @@ import QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 import {Course} from "./models/Course";
 import {Teacher} from "./models/Teacher";
 import {Homework} from "./models/Homework";
+import {Student} from "./models/Student";
 
 
 export type DocumentResultFixed = {
@@ -38,3 +39,19 @@ export interface TeacherCourseData {
   "teacher": Teacher
   "homeworksAndFiles": HomeworkAndUploadedFiles[]
 }
+
+export interface StudentOfTeacher {
+  course: string,
+  student: Student
+}
+
+export interface CoursesAndStudents {
+  course: string,
+  students: Student[]
+}
+
+export interface AllTeachersAndStudents {
+  teacher: Teacher,
+  coursesAndStudents: CoursesAndStudents[]
+}
+
