7f4c2064cabcec4a3fce7cedcf4d53fd4ad09965
Batuhan INAN <inanbatuhan61@gmail.com>
2022-02-04 15:41:52 +0300

    added bottom tab bar navigation

--- screens/PrincipalScreen.tsx
+++ screens/PrincipalScreen.tsx
@@ -1,40 +1,24 @@
-import { View } from '../components/Themed';
-import {TeacherCourseData} from '../types';
-import {useEffect, useState} from "react";
-import {getAllCoursesAndAllHomeworks} from "../utils/FirebaseStorageUtils";
-import {styles} from "../Styles";
-import {ActivityIndicator, BottomNavigation, List, Text} from "react-native-paper";
-import CourseListByStudent from "../components/CourseListByStudent";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Feather} from "@expo/vector-icons";
+import PrincipalScreenTeacherTab from "../components/PrincipalScreenTeacherTab";
+import PrincipalScreenHomeworkTab from "../components/PrincipalScreenHomeworkTab";
 
 
+const TabBarIcon = (props: {
+  name: React.ComponentProps<typeof Feather>['name'];
+  color: string;
+}) => {
+  return <Feather size={30} style={{ marginBottom: -3 }} {...props} />;
+}
 
+const Tab = createBottomTabNavigator();
 
 // @ts-ignore
-export default function PrincipalScreen({ navigation }) {
-
-  const [courses, setCourses] = useState<TeacherCourseData[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    getAllCoursesAndAllHomeworks(setCourses)
-      .then((_) => {
-        setIsLoading(false);
-      })
-  }, [])
-
-
-
-
+export default function PrincipalScreen() {
   return (
-    <View style={styles.container}>
-      <List.Section title={"Courses"}>
-        {courses.map((course) => {
-          return <CourseListByStudent key={course.course.name} course={course}/>
-        })}
-      </List.Section>
-      {isLoading &&
-        <ActivityIndicator animating={true} color="purple" size="large" />}
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="PrincipalScreenHomeworkTab" component={PrincipalScreenHomeworkTab} options={{ title: "Homeworks", tabBarIcon:  ({ color }) => <TabBarIcon name="bar-chart-2" color={color} /> }} />
+      <Tab.Screen name="PrincipalScreenTeacherTab" component={PrincipalScreenTeacherTab}   options={{ title: "Teachers", tabBarIcon:  ({ color }) => <TabBarIcon name="bar-chart-2" color={color} /> }}/>
+    </Tab.Navigator>
   );
 }
